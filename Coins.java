7b840bb61d2427453b83a0acbb336045ca085378
Guy Hasson <guy.hasson@post.runi.ac.il>
2024-01-25 22:57:15 +0200

    hw1

--- Coins.java
+++ Coins.java
@@ -8,6 +8,6 @@ public class Coins {
 		int total = Integer.parseInt(args[0]);
 		int quarters = total / 25;
 		int cents= total % 25;
-		System.out.println( " use " + quarters + " quarters and " + cents + " cents" );
+		System.out.println( " Use " + quarters + " quarters and " + cents + " cents" );
 	}
 }
